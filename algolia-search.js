59158c606c33beabd9a305c5aba490656c7ceef4
Alexandre Basta <42250730+docvinum@users.noreply.github.com>
2023-07-13 19:25:51 +0200

    Update algolia-search.js

--- algolia-search.js
+++ algolia-search.js
@@ -1,5 +1,3 @@
-/* global instantsearch algoliasearch */
-
 // Initialization of search instances for different Algolia indices
 const searchpfi = instantsearch({
   indexName: 'python_final_index',
@@ -246,152 +244,13 @@ searchInstance.addWidgets([
   }),
 ]);
 
-//ICS
-searchics.addWidgets([  
-  
-  instantsearch.widgets.configure({
-	hitsPerPage: 9,
-  }),
-  instantsearch.widgets.hits({
-      container: "#hits",
-	  cssClasses: {
-		root: 'ais_et_pb_blog_grid  articleHits',
-		list: ['column size-1of3'],
-		item: ['hit ']
-	  },
-      templates: {
-		empty: 'No posts found for <q>{{ query }}</q>',
-        item: function(item) {
-			
-			let baseDomain= 'https://ives-openscience.eu/';
-			if(item.url){
-				baseDomain = item.url;
-			}
- 			
-			
-			let tmpl = `
-			<article class="ais_et_pb_post clearfix"><h2 class="entry-title"><a href="${baseDomain}">${item.title}</a></h2>
-				<p class="post-meta">
-					Conference: <span class="published">${item.conference+" "+item.year}</span>   
-				</p>
-				<div class="post-content">
-					<div class="post-content-inner">
-						<p>${item.abstract}</p>
-					</div>
-				<a href="${baseDomain}" target="_blank" class="more-link">read more</a></div>
-				</article>`
-			return tmpl;
-          
-        }
-      }
-    }),
-  instantsearch.widgets.clearRefinements({
-		container: '#clear-refinements',
-		templates: {
-		resetLabel({ hasRefinements }, { html }) {
-		  return html`<span id="clearAisFilters">${hasRefinements ? 'Clear filters' : 'Filters'}</span>`;
-		},
-		},
-  }),
-  instantsearch.widgets.currentRefinements({
-		container: '#current-refinements',
-  }),
-  
-    refinementListWithPanel ({
-		container: '#ais-author',
-		attribute: 'author',
-		limit: 5,
-		showMore: true,
-		showMoreLimit: 20,
-		searchable: true,
-		searchablePlaceholder: 'Search author',
-		sortBy: ['count:desc', 'name:asc'],	
-		operator: 'or',
-	}),	
-	rLWithSource({
-		container: '#ais-source',
-		attribute: 'source',
-		limit: 5,
-		showMore: true,
-		showMoreLimit: 20,
-		searchable: true,
-		searchablePlaceholder: 'Search source',
-		sortBy: ['count:desc', 'name:asc'],	
-		operator: 'or',
-	}),
-	rLWithConference({
-		container: '#ais-conference',
-		attribute: 'conference',
-		sortBy: ['count:desc', 'name:asc'],	
-		operator: 'or',
-		limit: 5,
-		showMore: true,
-		showMoreLimit: 20,
-		searchable: true,
-		searchablePlaceholder: 'Search for conference',
-		searchableIsAlwaysActive: false,
-	}),
-	rLWithKeywords({
-		container: '#ais-keywords',
-		attribute: 'keywords',
-		limit: 5,
-		showMore: true,
-		showMoreLimit: 20,
-		searchable: true,
-		searchablePlaceholder: 'Search for Keywords',
-		sortBy: ['count:desc', 'name:asc'],
-		operator: 'or',
-	}),
-   
-    rLWithYear({
-		container: '#ais-year',
-		attribute: 'year',    
-		attributeName: 'year', 
-		limit: 5,
-		showMore: true,
-		showMoreLimit: 20,
-		searchable: true,
-		searchablePlaceholder: 'Search for Year',
-		sortBy: ['name:desc'],
-		operator: 'or',
-	}),
-
-	rLWithType({
-		container: '#ais-type',
-		attribute: 'type',
-		limit: 5,
-		showMore: true,
-		showMoreLimit: 20,
-		searchable: true,
-		searchablePlaceholder: 'Search for Type',
-		sortBy: ['count:desc', 'name:asc'],	
-		operator: 'or',
-	}),
-	
-	 
-   instantsearch.widgets.stats({
-    container: '#aisStats',
-    templates: {
-      text: `
-        {{#hasManyResults}} See {{nbHits}} Records {{/hasManyResults}}
-        {{#hasOneResult}} See 1 Record {{/hasOneResult}}
-        {{#hasNoResults}} 0 Records{{/hasNoResults}}
-      `
-    }
-  }),
-   
-  instantsearch.widgets.pagination({
-    container: '#pagination',
-  }),
-]);
-
 // Start search instance and setup event handlers
 function onRenderHandler() { }
 searchInstance.on('render', onRenderHandler);
 searchInstance.start();
 searchInstance.on('error', () => {});
 searchInstance.on('render', () => {
-  console.log(search.status === 'error' && search.error);
+  console.log(searchInstance.status === 'error' && searchInstance.error);
 });
 
 // Setup event handlers for filter overlay
